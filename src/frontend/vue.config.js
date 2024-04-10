fa084711b59f95e8b23df607afabd9ec62d31cdd
Alba Gómez <agl00108@red.ujaen.es>
2024-04-10 11:02:57 +0200

    Vue configurado

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
   devServer: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      '/TFG': {
+        target: 'http://localhost:4000',
         ws: true,
         changeOrigin: true
       }
