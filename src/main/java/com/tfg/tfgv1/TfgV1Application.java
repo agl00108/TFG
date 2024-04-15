94ed216539671d1fce347f730b8880137fa2cb09
Alba Gómez <agl00108@red.ujaen.es>
2024-04-15 12:48:06 +0200

    cambios proyecto vue

--- src/main/java/com/tfg/tfgv1/TfgV1Application.java
+++ src/main/java/com/tfg/tfgv1/TfgV1Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TfgV1Application {
+public class TfgV1Application
+{
 
     public static void main(String[] args) {
         SpringApplication.run(TfgV1Application.class, args);
