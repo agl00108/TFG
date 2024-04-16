161297e333b94a4c47cef7aa217682e6b315d241
Alba Gómez <agl00108@red.ujaen.es>
2024-04-16 13:25:39 +0200

    Intento de mapa

---
+++ src/frontend/frontend/src/components/GoogleMapLoader.vue
@@ -0,0 +1,40 @@
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+
+export default {
+  props: {
+    mapConfig: Object,
+    apiKey: String,
+  },
+  data() {
+    return {
+      google: null,
+      map: null
+    }
+  },
+  async mounted() {
+    const googleMapApi = await GoogleMapsApiLoader({
+      apiKey: this.apiKey
+    })
+    this.google = googleMapApi
+    this.initializeMap()
+  },
+
+  methods: {
+    initializeMap() {
+      const mapContainer = this.$refs.googleMap
+      this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="google-map" ref="googleMap"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/frontend/frontend/src/views/HomeView.vue
+++ src/frontend/frontend/src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup>
+<script>
+import Mapa from '../components/GoogleMapLoader.vue';
+export default
+{
+  components:
+      {
+    Mapa
+  }};
 </script>
 
 <template>
-  <main>
+  <h1>Página de Inicio</h1>
+  <Mapa></Mapa>
+<main>
   </main>
 </template>
