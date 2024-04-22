9cdce2f9dae2543cb0d445d9d664ab1ae0f50404
Alba Gomez Liebana <agl00108@red.ujaen.es>
2024-04-22 19:12:05 +0200

    cambios en indices

--- src/frontend/frontend/src/views/IndicesVegetacionView.vue
+++ src/frontend/frontend/src/views/IndicesVegetacionView.vue
@@ -6,18 +6,18 @@ const indices = ref([
   {
     nombre: 'NDVI (Normalized Difference Vegetation Index)',
     descripcion: 'Utiliza las bandas del rojo y el infrarrojo cercano y nos permite calcular el vigor de una planta en un estado fenológico concreto, el estado del cultivo y si la planta está sufriendo para poder llegar a tiempo y corregirlo.',
-    imagen: 'src/frontend/frontend/src/assets/img/NDVI.jpg'
+    imagen: '../assets/img/NDVI.jpg'
   },
   {
     nombre: 'NDWI (Normalized Difference Water Index)',
     descripcion: 'Compara la diferencia entre la banda del verde y la del infrarrojo cercano.\n' +
         'Es un índice apropiado para mapear cuerpos de agua y zonas anegadas, ya que el agua tiene una alta absorción y baja irradiación en el rango visible del infrarojo. Para este índice, los cuerpos de agua presentan valores mayores a 0, mientras que el suelo presenta valores menores a 0. No es un indicador de la humedad del suelo, pero si permite determinar el momento del ciclo en que se produce encharcamiento o anegamiento.\n',
-    imagen: 'src/frontend/frontend/src/assets/img/NDWI.jpg'
+    imagen: '../assets/img/NDWI.jpg'
   },
   {
     nombre: 'SAVI (Soil Adjusted Vegetation Index)',
     descripcion: 'Fue diseñado para minimizar las influencias del brillo del suelo. Su creador, Huete, agregó un factor de ajuste del suelo L a la ecuación de NDVI para corregir los efectos del ruido del suelo (color del suelo, humedad del suelo, variabilidad del suelo a través de la región, etc.), que tienden a afectar a los resultados. Este Índice de Vegetación se utiliza para el análisis de cultivos jóvenes, para regiones áridas con vegetación escasa (menos del 15% del área total) y superficies expuestas del suelo.',
-    imagen: 'src/frontend/frontend/src/assets/img/SAVI.jpg'
+    imagen: '../assets/img/SAVI.jpg'
   }
 ]);
 </script>
