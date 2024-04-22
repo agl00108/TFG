77fd82c3c47266bc208d97037a57001a5afa6eb7
Alba Gómez <agl00108@red.ujaen.es>
2024-04-22 13:38:57 +0200

    buscador en inicio

---
+++ src/frontend/frontend/src/components/IndiceVegetación.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="indice-vegetacion-details">
+    <h2>Índice de Vegetación</h2>
+    <div v-if="indiceVegetacion">
+      <p><strong>Nombre:</strong> {{ indiceVegetacion.nombre }}</p>
+      <p><strong>Descripción:</strong> {{ indiceVegetacion.descripcion }}</p>
+      <p><strong>Fecha de inicio:</strong> {{ indiceVegetacion.fechaInicio }}</p>
+      <p><strong>Fecha de fin:</strong> {{ indiceVegetacion.fechaFin }}</p>
+    </div>
+    <div v-else>
+      <p>Cargando datos del índice de vegetación...</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/frontend/frontend/src/views/HomeView.vue
+++ src/frontend/frontend/src/views/HomeView.vue
@@ -3,12 +3,58 @@ import LeafletMap  from "@/components/MapaFincasProvincia.vue";
 export default {
   components: {
     LeafletMap,
+  },
+methods: {
+
+  buscarFinca()
+  {
+    const formatoValido = /^[0-9]+:[0-9]+:[0-9]+:[0-9]+:[0-9]+:[0-9]+:[0-9]+$/.test(this.codigoSIGPAC);
+    const [provinciaCodigo, municipioCodigo, , , poligono, parcela, recinto] = this.codigoSIGPAC.split(':');
+    fetch(`/TFG/provincia/${provinciaCodigo}/municipio/${municipioCodigo}/finca/${poligono}/${parcela}/${recinto}`)
+        .then(response =>
+        {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();})
+        .then(data => {
+          this.$store.commit('setFinca', data);
+          this.$router.push({name: 'fincaEsp'});
+        })
+        .catch(error => {
+          console.error('Error en la solicitud:', error);
+          if (!formatoValido)
+          {
+            this.mostrarErrorFormato = true;
+            this.mostrarErrorEncontrado = false;
+          }
+          else
+          {
+            this.mostrarErrorFormato = false;
+            this.mostrarErrorEncontrado = true;
+          }
+        });
   }
 }
+};
 </script>
 
 <template>
+  <div class="search-form-container">
     <LeafletMap />
+    <form @submit.prevent="buscarFinca" class="search-form">
+      <div class="input-group">
+        <input v-model="codigoSIGPAC" type="text" class="form-control" placeholder="Introduce el código SIGPAC">
+        <button type="submit" class="btn btn-success"><i class="bi bi-search"></i> Buscar</button>
+      </div>
+      <div v-if="mostrarErrorFormato" class="error-message">
+        Formato SIGPAC no válido (provincia:municipio:agregado:zona:polígono:parcela:recinto)
+      </div>
+      <div v-if="mostrarErrorEncontrado" class="error-message">
+        Código SIGPAC no encontrado.
+      </div>
+    </form>
+  </div>
   <main>
   </main>
 </template>
