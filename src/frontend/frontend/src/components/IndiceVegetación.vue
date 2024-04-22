73260fd17258bd0fc7afd94c551e2275f522cc94
Alba Gomez Liebana <agl00108@red.ujaen.es>
2024-04-22 18:37:37 +0200

    enfermedades e indices

--- src/frontend/frontend/src/components/IndiceVegetación.vue
+++ src/frontend/frontend/src/components/IndiceVegetación.vue
@@ -1,22 +1,59 @@
-<script setup>
-
-</script>
-
 <template>
-  <div class="indice-vegetacion-details">
-    <h2>Índice de Vegetación</h2>
-    <div v-if="indiceVegetacion">
-      <p><strong>Nombre:</strong> {{ indiceVegetacion.nombre }}</p>
-      <p><strong>Descripción:</strong> {{ indiceVegetacion.descripcion }}</p>
-      <p><strong>Fecha de inicio:</strong> {{ indiceVegetacion.fechaInicio }}</p>
-      <p><strong>Fecha de fin:</strong> {{ indiceVegetacion.fechaFin }}</p>
+  <div class="indice-details">
+    <div class="indice-card" v-if="props.indice">
+      <h2>{{ props.indice.nombre }}</h2>
+      <p>{{ props.indice.descripcion }}</p>
+      <img :src="props.indice.imagen" alt="Formula" class="formula">
     </div>
-    <div v-else>
-      <p>Cargando datos del índice de vegetación...</p>
+    <div class="loading-message" v-else>
+      <p>Cargando datos de la enfermedad...</p>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script setup>
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  indice: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+.indice-details
+{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.indice-card
+{
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 400px;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease-in-out;
+}
+
+.indice-card:hover
+{
+  transform: translateY(-5px);
+}
+
+.loading-message
+{
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 400px;
+  text-align: center;
+}
 </style>
